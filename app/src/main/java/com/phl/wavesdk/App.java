9c263dd695d83588d80d2b30bac4f8bbeff6a197
cuieney1 <cuieney@163.com>
2017-02-14 10:54:29 +0800

    change
    change

--- app/src/main/java/com/phl/wavesdk/App.java
+++ app/src/main/java/com/phl/wavesdk/App.java
@@ -1,12 +1,8 @@
 package com.phl.wavesdk;
 
 import android.app.Application;
-import android.content.Context;
 
 import com.feetsdk.android.FeetSdk;
-import com.feetsdk.android.common.utils.Logger;
-import com.marswin89.marsdaemon.DaemonClient;
-import com.marswin89.marsdaemon.DaemonConfigurations;
 import com.squareup.leakcanary.LeakCanary;
 
 /**
@@ -27,46 +23,5 @@ public class App extends Application {
         LeakCanary.install(this);
     }
 
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        DaemonClient mDaemonClient = new DaemonClient(createDaemonConfigurations());
-        mDaemonClient.onAttachBaseContext(base);
-    }
-
-
-
-    private DaemonConfigurations createDaemonConfigurations(){
-        Logger.e("DaemonConfigurations");
-        DaemonConfigurations.DaemonConfiguration configuration1 = new DaemonConfigurations.DaemonConfiguration(
-                "com.marswin89.marsdaemon.demo:process1",
-                Service1.class.getCanonicalName(),
-                Receiver1.class.getCanonicalName());
-        DaemonConfigurations.DaemonConfiguration configuration2 = new DaemonConfigurations.DaemonConfiguration(
-                "com.marswin89.marsdaemon.demo:process2",
-                Service2.class.getCanonicalName(),
-                Receiver2.class.getCanonicalName());
-        DaemonConfigurations.DaemonListener listener = new MyDaemonListener();
-        //return new DaemonConfigurations(configuration1, configuration2);//listener can be null
-        return new DaemonConfigurations(configuration1, configuration2, listener);
-    }
-
-
-    class MyDaemonListener implements DaemonConfigurations.DaemonListener{
-        @Override
-        public void onPersistentStart(Context context) {
-            Logger.e("onPersistentStart");
-        }
-
-        @Override
-        public void onDaemonAssistantStart(Context context) {
-            Logger.e("onDaemonAssistantStart");
-        }
-
-        @Override
-        public void onWatchDaemonDaed() {
-            Logger.e("onWatchDaemonDaed");
-        }
-    }
 
 }
